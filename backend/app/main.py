b4ab7ecf5e15ab31f8f7ee02a99f878f0b08da15
sharath <sharathraghav2003@gmail>
2026-02-09 15:36:42 +0530

    fixed CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,21 @@ from fastapi import FastAPI
 from app.routes.tasks import router as tasks_router
 from app.health import router as health_router
 from app.metrics import router as metrics_router
+from fastapi.middleware.cors import CORSMiddleware
 
+# 1️⃣ Create app first
 app = FastAPI(title="Cloud Native Task API")
 
+# 2️⃣ Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# 3️⃣ Include routers
 app.include_router(tasks_router)
 app.include_router(health_router)
 app.include_router(metrics_router)
